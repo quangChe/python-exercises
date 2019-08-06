87c28af8c4e5035ac668ffd69fc7c24c816941ec
quangChe <quang.n.che@gmail.com>
2019-08-05 21:53:11 -0700

    Cleaned up and refactored guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -2,20 +2,33 @@ import random
 
 def Game():
   '''
-  A simple guessing game where players have to guess and input a 3-digit integer
-  into the prompt
+  A simple guessing game where players have to guess and input a 3-digit 
+  integer into the prompt with following rules:
+
+    - A 'Match' feedback is when user has at least one number guessed 
+      correctly and on the right place value (ones, tens, or hundreds
+      place)
+    - A 'Close' feedback is when user has at least one number guessed
+      correctly but it/they might not be on the correct place value
+    - A 'Nope' feedback is when user has no matches 
+  '''
+  print("Welcome Code Breaker! Let's see if you can guess my 3 digit number!")
+  print("Code has been generated, please guess a 3 digit number")
+  solution = generate_solution()
+  run_game_prompt(solution)
+
+
+def generate_solution():
+  '''
+  Generates a random 3-digit number to be used as a solution to the game
   '''
   digits = list(range(10))
   random.shuffle(digits)
-  solution = digits[:3]
-  print("Welcome Code Breaker! Let's see if you can guess my 3 digit number!")
-  print("Code has been generated, please gues a 3 digit number")
-  promptInput(solution)
+  return digits[:3]
 
 def match(g, a):
   '''
-  A 'Match' in the game is when user has at least one number guessed correctly
-  and on the right place value (ones, tens, or hundreds place)
+  Checks a 'Match' game condition
   '''
   for i in range(len(g)):
     if g[i] == a[i]:
@@ -24,33 +37,32 @@ def match(g, a):
 
 def close(g, a):
   '''
-  A 'Close' in the game is when user has at least one number guessed correctly
-  but it/they might not be on the correct place value
+  Checks a 'Close' game condition
   '''
   for num in g:
     if num in a:
       return True
   return False
 
-def promptInput(answer):
+def run_game_prompt(answer):
   '''
   Recurscive logic of the game, keep prompting until correct
   '''
   guess = [int(item) for item in list(input("What is your guess? ")[:3])]
   if guess == answer:
-    user_feedback = input("~~~~YOU WIN!!!~~~~ Play again? [y/n]")
+    user_feedback = input("~~~~YOU WIN!!!~~~~ Play again? [y/n]:")
     if user_feedback[0] == 'y':
-      return Game()
+      run_game_prompt(generate_solution())
     else:
-      print("Thanks for playing. Goodbye!")
+      return print("Thanks for playing. Goodbye!")
   elif match(guess, answer):
     print('Match!')
-    return promptInput(answer)
+    return run_game_prompt(answer)
   elif close(guess, answer):
     print('Close!')
-    return promptInput(answer)
+    return run_game_prompt(answer)
   else:
     print('Nope!')
-    return promptInput(answer)
+    return run_game_prompt(answer)
 
 Game()
